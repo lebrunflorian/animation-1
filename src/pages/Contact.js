1e3bf01ae8353a5ce78b961908d397dec98a64a0
Florian <k4p4t4u@gmail.com>
2020-06-16 14:08:23 +0200

    Commit initial

---
+++ src/pages/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="contact">
+      <div className="container">
+        <div className="row">
+          <div className="inner">
+            <div>
+              <h2>hello@lebrunflorian.info</h2>
+            </div>
+            <br />
+            <ul>
+              <li>
+                <a href="https://twitter.com/flebrun_">Twitter</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/lebrunflorian/">
+                  Linkedin
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
